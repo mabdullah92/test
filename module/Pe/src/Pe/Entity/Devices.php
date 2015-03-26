091f3e351c06e5917c44e0f3287c7645412a7aac
mabdullah92 <mabdullahjavaid93@gmail.com>
2015-03-26 18:32:10 +0500

    Date wise Search implemented in data tables

---
+++ module/Pe/src/Pe/Entity/Devices.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 3/26/2015
+ * Time: 6:30 PM
+ */ 
--- module/Pe/src/Pe/Model/ReadingsModel.php
+++ module/Pe/src/Pe/Model/ReadingsModel.php
@@ -11,24 +11,14 @@ class ReadingsModel
 
     public function  stromSelectAll($dm,$data)
     {
-
         $param=$data;
-        echo $param["fdate"]." - ".$param["tdate"];
-        $query="select u from Pe\\Entity\\Readings u where u.read_Date >=";
-
-        if($param['fdate']!=null)
-        { $query .= '\''. date('Y-m-d',strtotime($param['fdate'])).'\'';}
-        $query.=" and u.read_Date <= ";
-        if($param['tdate']!=null)
-        { $query .='\''. date(' Y-m-d',strtotime($param['tdate'])).'\'';}
+        $query="select u from Pe\\Entity\\Readings u";
+        if($param['fdate']!='')
+        { $query .= ' where u.read_Date >= \''. date('Y-m-d',strtotime($param['fdate'])).'\'';}
+        if($param['tdate']!='')
+        { $query .=' and u.read_Date <= \''. date(' Y-m-d',strtotime($param['tdate'])).'\'';}
         $data=$dm->createQuery($query)->getResult();
-   //     $data=$data->getResult();
 
-//        $to='12-12-2010';
-//        $from='12-12-2019';
-//        $data = $dm->getRepository('Pe\Entity\Readings')->createQueryBuilder('c')->findBy('Id=186')
-//            ->getQuery()
-//            ->execute();
         foreach ($data as $row) {
            $d=$row->getread_DateTime()->format('Y/m/d H:i:s');
             $arr["data"][] = array(
@@ -49,22 +39,8 @@ class ReadingsModel
 
     public function  stromFind($dm, $data)
     {
-        $param = $data;
-        $data = $dm->getRepository('Pe\Entity\Readings')->findBy(array("id" => $param));
-        foreach ($data as $row) {
-            $arr["data"][] = array(
-                "Id" => $row->getId(),
-                "Device_Id" => $row->getDevice_Id(),
-                "Temperature" => $row->getTemperature(),
-                "Humidity" => $row->getHumidity(),
-                "read_DateTime" => $row->getread_DateTime(),
-                "read_Date" => $row->getread_Date(),
-                "read_Time" => $row->getread_Time()
-            );
-        }
-        $arr["tableName"]="Readings";
-        $arr["columns"]=array("Shed Name","Date & Time","Temperature","Humidity");
-        return $arr;
+
+
     }
 
     public function  stromEdit($dm, $data)
